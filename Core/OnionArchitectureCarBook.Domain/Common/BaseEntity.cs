7a7a38ca6d49e1cc92f423d23176b5b1c58295a1
beko10 <berkay1905ber@gmail.com>
2025-05-13 17:11:46 +0300

    Base Entity Oluşturuldu

---
+++ Core/OnionArchitectureCarBook.Domain/Common/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace OnionArchitectureCarBook.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public string Id { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedDate { get; set; } = DateTime.UtcNow;
+    public bool IsDeleted { get; set; } = false;
+
+    protected BaseEntity()
+    {
+        Id = Guid.NewGuid().ToString();
+    }
+}
