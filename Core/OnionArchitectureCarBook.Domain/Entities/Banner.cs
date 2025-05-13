8cf7b9dd6f5ed0e2046ac1429b326be91608b51b
beko10 <berkay1905ber@gmail.com>
2025-05-13 17:14:56 +0300

    Banner Varlığı Eklendi

---
+++ Core/OnionArchitectureCarBook.Domain/Entities/Banner.cs
@@ -0,0 +1,17 @@
+﻿using OnionArchitectureCarBook.Domain.Common;
+
+namespace OnionArchitectureCarBook.Domain.Entities;
+
+public sealed class Banner : BaseEntity
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string VideoUrl { get; set; }
+
+    public Banner()
+    {
+        Title = string.Empty;
+        Description = string.Empty;
+        VideoUrl = string.Empty;
+    }
+}
